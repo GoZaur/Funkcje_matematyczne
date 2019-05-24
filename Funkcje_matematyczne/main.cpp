15482c2b9c0a467475b5bb578e48aacca6d2a374
Gosia Gryziak <gosiagryziak7606@gmail.com>
2019-05-24 14:37:39 +0200

    funkcje_matematyczne

--- Funkcje_matematyczne/main.cpp
+++ Funkcje_matematyczne/main.cpp
@@ -4,22 +4,26 @@
 #include<cstdlib>
 #include"Silnia.h"
 #include"NWD.h"
+#include"Rownanie_kwadratowe.h"
+#include<math.h>
 
 using namespace std;
 
 int liczba;
 int main() {
 	setlocale(LC_CTYPE, "polish");
+
 	cout << "Witaj! Podaj liczbê, któr¹ chcesz sprawdziæ czy jest pierwsza: " << liczba << endl;
-	bool czy_liczba_pierwsza();
+	bool pierwsza = czy_liczba_pierwsza(7);
 	//cout<<"Podana liczba to: "<<
 
 	nwd(12, 15);
 	cout << "Najwiêkszy wspólny dzielnik dla podanych liczb, to: " << endl;//tutaj muszê wczytaæ 
 
-	silnia(4);
+	silnia(3);
 	cout << "Silnia podanej liczby wynosi: " << endl;//wczytaæ
 
+	miejsca_zerowe(a, b, c);
 	system("Pause");
 
 }
